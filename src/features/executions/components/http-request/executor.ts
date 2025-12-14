6698b5a4bac03a45db6bd5daa8a9960e8b8a83a7
arun-upadhayay <arunupadhayay10102000@gmail.com>
2025-12-14 13:18:03 +0530

    14|12|25: add variable name in http request from modal

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -5,6 +5,7 @@ import { NodeExecutor } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 
 type HttpRequestData = {
+  variableName?: string;
   endpoint?: string;
   method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
   body?: string;
@@ -22,6 +23,10 @@ export const HttpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
     // TODO: Publish "error" state for http request
     throw new NonRetriableError("HTTP request node: No endpoint configured");
   }
+  if (!data.variableName) {
+    // TODO: Publish "error" state for http request
+    throw new NonRetriableError("Variable name not configured");
+  }
 
   const result = await step.run("http-request", async () => {
     const endpoint = data.endpoint!;
@@ -35,6 +40,9 @@ export const HttpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
       //     options.body = data.body;
       //   }
       options.body = data.body;
+      options.headers = {
+        "Content-Type": "application/json",
+      };
     }
     const response = await Ky(endpoint, options);
     const contentType = response.headers.get("content-type");
@@ -42,13 +50,24 @@ export const HttpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
       ? await response.json()
       : await response.text();
 
-    return {
-      ...context,
+    const responsePayload = {
       httpResponse: {
         data: responseData,
         status: response.status,
         statusText: response.statusText,
       },
+    };
+
+    if (data.variableName) {
+      return {
+        ...context,
+        [data.variableName]: responsePayload,
+      };
+    }
+    // Fallback to direct http response fpr backward compatibility
+    return {
+      ...context,
+      ...responsePayload,
     };
   });
 
