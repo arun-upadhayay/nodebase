eac15704f64b3fe1f435ee61798a8d434736b7fc
arun-upadhayay <arunupadhayay10102000@gmail.com>
2025-12-25 19:18:13 +0530

    25|12|25: add a google form node

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -6,6 +6,7 @@ import { PAGINATION } from "@/config/constants";
 import { NodeType } from "@/generated/prisma";
 import type { Node, Edge } from "@xyflow/react";
 import { inngest } from "@/inngest/client";
+import { sendWorkflowExecution } from "@/inngest/utils";
 
 export const workflowsRouter = createTRPCRouter({
   execute: protectedProcedure
@@ -18,13 +19,15 @@ export const workflowsRouter = createTRPCRouter({
         },
       });
 
-      await inngest.send({
-        name: "workflows/execute.workflow",
-        data: {
-          workflowId: input.id,
-          
-        },
-      });
+      // await inngest.send({
+      //   name: "workflows/execute.workflow",
+      //   data: {
+      //     workflowId: input.id,
+
+      //   },
+      // });
+      
+      await sendWorkflowExecution({ workflowId: input.id });
 
       return workflow;
     }),
